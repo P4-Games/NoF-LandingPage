37853a9101f2683bef5038bbf871be26d48bf5f3
NextDoor <nestorhuircapan25@gmail.com>
2022-11-01 08:31:02 -0300

    Delete OneDrive/Escritorio/GitHub/web-3.0 directory
    Error al querer subir Archivos de la landing de NoF. borro los repositorios para subir los que deberían subirse.

--- OneDrive/Escritorio/GitHub/web-3.0/client/src/components/Services.jsx
+++
@@ -1,7 +0,0 @@
-const Services = () => {
-    return (
-        <h1>Services</h1>
-    );
-}
-
-export default Services;
--- OneDrive/Escritorio/GitHub/web-3.0/client/src/components/Transactions.jsx
+++
@@ -1,7 +0,0 @@
-const Transactions = () => {
-    return (
-        <h1>Transactions</h1>
-    );
-}
-
-export default Transactions;
