05232684e125e683fa9ab5519ef10b157185e2e2
Tomas <tomasfreire@gmail.com>
2022-12-22 22:11:53 -0300

    re added alpha css import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,7 @@
 import '../styles/index.scss'
-// import 'sweetalert2/src/sweetalert2.scss'
+import '../styles/alpha.scss'
+import 'sweetalert2/src/sweetalert2.scss'
+
 
 function MyApp({ Component, pageProps }) {
   return (
