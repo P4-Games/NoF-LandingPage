76a65d6ee947dadf8130292b928273a12cc1df6e
Martin H. Pefaur <mpefaur@gmail.com>
2023-01-12 23:00:46 -0500

    Fix palancas
    Fix palancas del footer y colecciones del navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,55 +1,53 @@
+import React, { useRef } from 'react'
 import Head from 'next/head'
-// import Navbar from '../components/Navbar'
-// import Hero from './sections/Hero'
+import Navbar from '../components/Navbar'
+import Hero from './sections/Hero'
 import Footer from '../components/Footer'
-import { useRef } from 'react'
 import useTranslation from "../components/Navbar/useTranslation"
-import dynamic from 'next/dynamic'
-const Navbar = dynamic(() => import("../components/Navbar"), { ssr: false })
-const Hero = dynamic(() => import("./sections/Hero"), { ssr: false })
 
 function Home() {
 
-  const book = useRef()
-  function turnNextPage() {
-    book.current.pageFlip().flipNext()
-  }
-  function turnPrevPage() {
-    book.current.pageFlip().flipPrev()
-  }
-  function onFlip() {
-    page.onFlip()
-  }
-  function goToCollections(number) {
-    book.current.pageFlip().flip(number)
-  }
-  const { language, setLanguage, setFallbackLanguage, t } = useTranslation();
+const book = useRef(null)
+function turnNextPage() {
+book.current.pageFlip().flipNext()
+}
+function turnPrevPage() {
+book.current.pageFlip().flipPrev()
+}
+function onFlip() {
+page.onFlip()
+}
+function goToCollections(number) {
+book.current.pageFlip().flip(number)
+}
+const { language, setLanguage, setFallbackLanguage, t } = useTranslation();
 
-  return (
-    <div className='home'>
-      <Head>
-        <title>Number One Fan</title>
-        <meta name='description' content='Number One Fan' />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <link rel='icon' href='./favicon.ico' />
-      </Head>
-      <Navbar onFlip={onFlip} goToCollections={goToCollections}
-        language={language}
-        setLanguage={setLanguage}
-        t={t} />
-      <Hero
-        ref={book}
-        language={language}
-        setLanguage={setLanguage}
-        t={t}
-
-      />
-      <Footer
-        turnNextPage={turnNextPage}
-        turnPrevPage={turnPrevPage}
-      />
-    </div>
-  )
+return (
+<div className='home'>
+<Head>
+<title>Number One Fan</title>
+<meta name='description' content='Number One Fan' />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+<link rel='icon' href='./favicon.ico' />
+</Head>
+<Navbar onFlip={onFlip} goToCollections={goToCollections}
+     language={language}
+     setLanguage={setLanguage}
+     t={t} />
+<Hero
+     ref={book}
+     language={language}
+     setLanguage={setLanguage}
+     t={t}
+     turnNextPage={turnNextPage}
+     turnPrevPage={turnPrevPage}
+   />
+<Footer
+     turnNextPage={turnNextPage}
+     turnPrevPage={turnPrevPage}
+   />
+</div>
+)
 }
 
 export default Home
