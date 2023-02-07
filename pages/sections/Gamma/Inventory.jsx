0637f76023de0aebc8ad555176b5bc2cd286dfcc
Gabriel Vazquez <gavf.vazquez@gmail.com>
2023-02-07 11:58:38 -0300

    Add book with to initial pages

--- pages/sections/Gamma/Inventory.jsx
+++ pages/sections/Gamma/Inventory.jsx
@@ -4,33 +4,88 @@ import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
 import HTMLFlipBook from "react-pageflip";
 
-const GammaInventory = React.forwardRef((props, book)) => (
-  <div className="gamma_main">
-    <Head>
-      <title>Number One Fan</title>
-      <meta name="description" content="NoF Gamma" />
-      <link rel="icon" href="./favicon.ico" />
-    </Head>
-    <Navbar />
-    <HTMLFlipBook
-      id="Book"
-      size={"stretch"}
-      width={360}
-      height={500}
-      minWidth={350}
-      maxWidth={800}
-      minHeight={500}
-      maxHeight={800}
-      autoSize={true}
-      ref={book}
-      usePortrait={size}
-      drawShadow={false}
-      className="hero__top__album__book"
-    >
-      <div>Prueba</div>
-    </HTMLFlipBook>
-    <Footer />
-  </div>
-);
+const GammaInventory = React.forwardRef((props, book) => {
+  const [mobile, setMobile] = useState(false);
+  const [size, setSize] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 600) {
+      setMobile(true);
+      setSize(true);
+    } else {
+      setMobile(false);
+      setSize(false);
+    }
+    const updateMedia = () => {
+      if (window.innerWidth < 600) {
+        setMobile(true);
+        setSize(true);
+      } else {
+        setMobile(false);
+        setSize(false);
+      }
+    };
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
+  return (
+    <div className="gamma_main">
+      <Head>
+        <title>Number One Fan</title>
+        <meta name="description" content="NoF Gamma" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <Navbar />
+      <div className="hero__top">
+        <div className="hero__top__album">
+          <HTMLFlipBook
+            id="Book"
+            size={"stretch"}
+            width={360}
+            height={500}
+            minWidth={350}
+            maxWidth={800}
+            minHeight={500}
+            maxHeight={800}
+            autoSize={true}
+            drawShadow={false}
+            usePortrait={size}
+            ref={book}
+            className="hero__top__album__book"
+          >
+            <div
+              className="hero__top__album__book__page"
+              data-density="hard"
+              number="1"
+            >
+              <div className="hero__top__album__book__page__page-content">
+                Prueba
+              </div>
+            </div>
+            <div
+              className="hero__top__album__book__page0"
+              data-density="hard"
+              number="2"
+            >
+              <div className="hero__top__album__book__page__page-content">
+                Prueba2
+              </div>
+            </div>
+            <div
+              className="hero__top__album__book__page"
+              data-density="hard"
+              number="3"
+            >
+              <div className="hero__top__album__book__page__page-content">
+                Prueba3
+              </div>
+            </div>
+          </HTMLFlipBook>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+});
 
 export default GammaInventory;
