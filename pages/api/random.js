25f603cacc4a4291d7967777e0c1a7ef5f08ecb1
Martin H. Pefaur <mpefaur@gmail.com>
2023-05-09 18:21:41 -0300

    fix random
    devuelve lista con 1 elemento.

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,5 +1,5 @@
 export default (req, res) => {
     const randomInt = Math.floor(Math.random() * 120);
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(randomInt);
+    res.status(200).json([randomInt]);
   };
