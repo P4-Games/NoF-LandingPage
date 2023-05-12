fe9b12a4c05fb342787c758f551506a146ea80e7
Martin H. Pefaur <mpefaur@gmail.com>
2023-05-12 14:00:45 -0300

    fix newCharacter
    try to fix, no estaria funcionando, intento de mejora.

--- pages/api/newCharacter.js
+++ pages/api/newCharacter.js
@@ -3,61 +3,61 @@ import path from 'path';
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  if (req.method === 'PUT') {
-    try {
-      const { discordID, characterID } = req.body;
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
-      // Leer el archivo users.json desde la API de jsonbin.io
-      const response = await axios.get('https://api.jsonbin.io/v3/b/645e3e318e4aa6225e9b9ed0/latest', {
-        headers: {
-          'X-Master-Key': '$2b$10$F0trbhCY6YrpjDlyC3lilu8xwycimMfmenE.ak1eC5nhHrXX/g5B6'
-        }
-      });
+  try {
+    const { discordID, characterID } = req.body;
 
-      const users = response.data.record;
-
-      // Buscar el usuario correspondiente al discordID
-      const user = users.find(u => u.discordID === discordID);
-
-      if (!user) {
-        return res.status(400).json({ message: 'User not found' });
+    // Obtener los usuarios desde jsonbin.io
+    const response = await axios.get('https://api.jsonbin.io/v3/b/645e3e318e4aa6225e9b9ed0/latest', {
+      headers: {
+        'X-Master-Key': "$2b$10$F0trbhCY6YrpjDlyC3lilu8xwycimMfmenE.ak1eC5nhHrXX/g5B6"
       }
+    });
 
-      // Verificar si el personaje ya está en el inventario del usuario
-      const character = user.characters.find(c => c.id === characterID);
+    const users = response.data.record;
 
-      if (character) {
-        return res.status(400).json({ message: 'Ya tienes este personaje en tu inventario' });
-      }
+    // Buscar el usuario correspondiente al discordID
+    const user = users.find(u => u.discordID === discordID);
 
-      // Leer el archivo characters.json desde el directorio raíz del proyecto
-      const charactersPath = path.join(process.cwd(), 'characters.json');
-      const charactersData = fs.readFileSync(charactersPath);
-      const characters = JSON.parse(charactersData);
-
-      // Buscar el personaje correspondiente al characterID
-      const newCharacter = characters.find(c => c.id === characterID);
-
-      if (!newCharacter) {
-        return res.status(400).json({ message: 'Character not found' });
-      }
-
-      // Agregar el nuevo personaje al inventario del usuario
-      user.characters.push({ id: newCharacter.id, image: newCharacter.image });
-
-      // Actualizar el archivo users.json en jsonbin.io
-      await axios.put('https://api.jsonbin.io/v3/b/645e3e318e4aa6225e9b9ed0', { record: users }, {
-        headers: {
-          'X-Master-Key': '$2b$10$F0trbhCY6YrpjDlyC3lilu8xwycimMfmenE.ak1eC5nhHrXX/g5B6'
-        }
-      });
-
-      res.status(200).json({ message: 'Personaje agregado correctamente' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Server error' });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
     }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
+
+    // Verificar si el personaje ya está en el inventario del usuario
+    const character = user.characters.find(c => c.id === characterID);
+
+    if (character) {
+      return res.status(400).json({ message: 'Ya tienes este personaje en tu inventario' });
+    }
+
+    // Obtener los personajes desde un archivo local
+    const charactersPath = path.join(process.cwd(), 'characters.json');
+    const charactersData = fs.readFileSync(charactersPath);
+    const characters = JSON.parse(charactersData);
+
+    // Buscar el personaje correspondiente al characterID
+    const newCharacter = characters.find(c => c.id === characterID);
+
+    if (!newCharacter) {
+      return res.status(400).json({ message: 'Character not found' });
+    }
+
+    // Agregar el nuevo personaje al inventario del usuario
+    user.characters.push({ id: newCharacter.id, image: newCharacter.image });
+
+    // Actualizar los usuarios en jsonbin.io
+    await axios.put('https://api.jsonbin.io/v3/b/645e3e318e4aa6225e9b9ed0', { record: users }, {
+      headers: {
+        'X-Master-Key': "$2b$10$F0trbhCY6YrpjDlyC3lilu8xwycimMfmenE.ak1eC5nhHrXX/g5B6"
+      }
+    });
+
+    res.status(200).json({ message: 'Personaje agregado correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 }
