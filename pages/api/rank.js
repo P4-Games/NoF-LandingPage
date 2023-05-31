9b88c33fd9a20a34d7043bcaa2e64e087788081f
Martin H. Pefaur <mpefaur@gmail.com>
2023-05-31 14:52:01 -0300

    Update Rank
    Agrego ranking por medallas.

--- pages/api/rank.js
+++ pages/api/rank.js
@@ -5,22 +5,79 @@ const getUsersRank = async () => {
   const db = await connectToDatabase();
   const usersCollection = db.collection('users');
 
-  const userRanking = await usersCollection.aggregate([
-    { $project: { nick: 1, characters: { $size: "$characters" } } },
-    { $sort: { characters: -1 } },
-    { $limit: 10 }
-  ]).toArray();
+  const userRanking = await usersCollection.find().toArray();
 
-  const rankingList = userRanking.map((user, index) => {
+  const sortedRanking = userRanking.sort((a, b) => {
+    const aMedalRank = calculateMedalRank(a.medals);
+    const bMedalRank = calculateMedalRank(b.medals);
+
+    if (aMedalRank > bMedalRank) {
+      return -1;
+    } else if (aMedalRank < bMedalRank) {
+      return 1;
+    } else {
+      if (aMedalRank === 3 && bMedalRank === 3) {
+        // Ambos tienen medallas de oro, comparar por cantidad de medallas de oro
+        const aGoldMedals = countMedals(a.medals, 'gold');
+        const bGoldMedals = countMedals(b.medals, 'gold');
+
+        if (aGoldMedals > bGoldMedals) {
+          return -1;
+        } else if (aGoldMedals < bGoldMedals) {
+          return 1;
+        } else {
+          // Ambos tienen la misma cantidad de medallas de oro, comparar por cantidad de personajes
+          if (a.characters.length > b.characters.length) {
+            return -1;
+          } else if (a.characters.length < b.characters.length) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      } else {
+        // No hay medallas de oro en ambos usuarios, comparar por cantidad de personajes
+        if (a.characters.length > b.characters.length) {
+          return -1;
+        } else if (a.characters.length < b.characters.length) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    }
+  });
+
+  const rankingList = sortedRanking.map((user, index) => {
     return {
       Position: index + 1,
       Nick: user.nick,
-      Characters: user.characters
+      Characters: user.characters.length,
+      Medals: user.medals
     };
   });
 
   return rankingList;
 };
+
+const calculateMedalRank = (medals) => {
+  if (medals.includes('gold')) {
+    return 3;
+  } else if (medals.includes('silver')) {
+    return 2;
+  } else if (medals.includes('bronze')) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const countMedals = (medals, medalType) => {
+  return medals.filter(medal => medal === medalType).length;
+};
+
+
+
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
