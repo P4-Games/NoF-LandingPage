b7e66fb5a8bdc154ce467bbaa6a54fd053730fad
Martin H. Pefaur <mpefaur@gmail.com>
2023-06-02 12:38:02 -0300

    update info.js
    ahora devuelve el rank real del usuario teniendo en cuenta las medallas.-

--- pages/api/info.js
+++ pages/api/info.js
@@ -19,12 +19,9 @@ const getUserInfo = async (db, discordID) => {
     { $group: { _id: null, count: { $sum: 1 } } }
   ]).toArray();
 
-  const rankingCursor = await usersCollection.aggregate([
-    { $project: { _id: 0, characters: { $size: "$characters" } } },
-    { $sort: { characters: -1 } }
-  ]);
-  const userRanking = await rankingCursor.toArray();
-  const userPosition = userRanking.findIndex(item => item.characters === charactersCount) + 1;
+  const userRanking = await usersCollection.find().toArray();
+  const sortedRanking = calculateRank(userRanking);
+  const userPosition = sortedRanking.findIndex(item => item.nick === user.nick) + 1;
 
   return {
     Nick: user.nick,
@@ -35,6 +32,60 @@ const getUserInfo = async (db, discordID) => {
     CharactersCaptured: charactersCaptured[0].count,
     Ranking: userPosition
   };
+};
+
+const calculateRank = (users) => {
+  return users.sort((a, b) => {
+    const aMedalRank = calculateMedalRank(a.medals);
+    const bMedalRank = calculateMedalRank(b.medals);
+
+    if (aMedalRank > bMedalRank) {
+      return -1;
+    } else if (aMedalRank < bMedalRank) {
+      return 1;
+    } else {
+      if (aMedalRank === 3 && bMedalRank === 3) {
+        const aGoldMedals = countMedals(a.medals, 'gold');
+        const bGoldMedals = countMedals(b.medals, 'gold');
+
+        if (aGoldMedals > bGoldMedals) {
+          return -1;
+        } else if (aGoldMedals < bGoldMedals) {
+          return 1;
+        } else {
+          return compareCharacters(a, b);
+        }
+      } else {
+        return compareCharacters(a, b);
+      }
+    }
+  });
+};
+
+const calculateMedalRank = (medals) => {
+  if (medals.includes('gold')) {
+    return 3;
+  } else if (medals.includes('silver')) {
+    return 2;
+  } else if (medals.includes('bronze')) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const countMedals = (medals, medalType) => {
+  return medals.filter(medal => medal === medalType).length;
+};
+
+const compareCharacters = (a, b) => {
+  if (a.characters.length > b.characters.length) {
+    return -1;
+  } else if (a.characters.length < b.characters.length) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
 
 export default async function handler(req, res) {
