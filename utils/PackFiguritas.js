463fed6ecc3473845fa0694acb747a7ba734b10f
dappsar <dapps.ar@gmail.com>
2023-10-30 20:12:29 -0300

    commented not-used variable

--- utils/PackFiguritas.js
+++ utils/PackFiguritas.js
@@ -200,8 +200,8 @@ function PackFiguritas({ openPackage, cardsNumbers }) {
       document.querySelector('.boxes').addEventListener('click', (e) => {
         const BOX = e.target.closest('.box');
         if (BOX) {
-          let TARGET = BOXES.indexOf(BOX);
-          let CURRENT = gsap.utils.wrap(
+          const TARGET = BOXES.indexOf(BOX);
+          const CURRENT = gsap.utils.wrap(
             0,
             BOXES.length,
             Math.floor(BOXES.length * SCRUB.vars.position)
@@ -276,7 +276,7 @@ function PackFiguritas({ openPackage, cardsNumbers }) {
   function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
   }
-  const randomNumber = generateRandomNumber();
+  // const randomNumber = generateRandomNumber();
 
   return (
     <>
@@ -312,13 +312,13 @@ function PackFiguritas({ openPackage, cardsNumbers }) {
           <button className="next">
             <span>Previous album</span>
             <svg viewBox="0 0 448 512" width="100" title="Previous Album">
-              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
             </svg>
           </button>
           <button className="prev">
             <span>Next album</span>
             <svg viewBox="0 0 448 512" width="100" title="Next Album">
-              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
             </svg>
           </button>
         </div>
@@ -326,7 +326,7 @@ function PackFiguritas({ openPackage, cardsNumbers }) {
       {/* <svg className="scroll-icon" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M20 6H23L19 2L15 6H18V18H15L19 22L23 18H20V6M9 3.09C11.83 3.57 14 6.04 14 9H9V3.09M14 11V15C14 18.3 11.3 21 8 21S2 18.3 2 15V11H14M7 9H2C2 6.04 4.17 3.57 7 3.09V9Z"></path>
             </svg> */}
-      <div className="drag-proxy"></div>
+      <div className="drag-proxy" />
     </>
   );
 }
