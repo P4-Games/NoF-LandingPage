93df584ef31ab5c8c8c03ae18be283316e8151ad
dappsar <dapps.ar@gmail.com>
2023-10-30 20:11:25 -0300

    commented not defined page object

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,36 +1,36 @@
-import React, { useRef } from "react";
-import Head from "next/head";
-import Navbar from "../components/Navbar";
-import Hero from "./sections/Hero";
-import Footer from "../components/Footer";
-import useTranslation from "../components/Navbar/useTranslation";
+import React, { useRef } from 'react'
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import Hero from './sections/Hero'
+import Footer from '../components/Footer'
+import useTranslation from '../components/Navbar/useTranslation'
 
-function Home() {
-  const book = useRef(null);
-  function turnNextPage() {
-    book.current.pageFlip().flipNext();
+function Home () {
+  const book = useRef(null)
+  function turnNextPage () {
+    book.current.pageFlip().flipNext()
   }
-  function turnPrevPage() {
-    book.current.pageFlip().flipPrev();
+  function turnPrevPage () {
+    book.current.pageFlip().flipPrev()
   }
-  function onFlip() {
-    page.onFlip();
+  function onFlip () {
+    page.onFlip()
   }
-  function goToCollections(number) {
-    book.current.pageFlip().flip(number);
+  function goToCollections (number) {
+    book.current.pageFlip().flip(number)
   }
-  const { language, setLanguage, setFallbackLanguage, t } = useTranslation();
+  const { language, setLanguage, setFallbackLanguage, t } = useTranslation()
 
   return (
-    <div className="home">
+    <div className='home'>
       <Head>
         <title>Number One Fan</title>
-        <meta name="description" content="Number One Fan" />
+        <meta name='description' content='Number One Fan' />
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <link rel="icon" href="./favicon.ico" />
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        />
+        <link rel='icon' href='./favicon.ico' />
       </Head>
       <Navbar
         onFlip={onFlip}
@@ -49,7 +49,7 @@ function Home() {
       />
       <Footer turnNextPage={turnNextPage} turnPrevPage={turnPrevPage} />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
