4f133ba6fae66b460c8a0d84a503b248d02e3128
dappsar <dapps.ar@gmail.com>
2023-10-30 23:02:37 -0300

    homogenized the switchOrCreateNetwork function

--- components/AlphaCards.jsx
+++ components/AlphaCards.jsx
@@ -78,7 +78,13 @@ const AlphaCards = ({ loadAlbums, setLoadAlbums, alphaMidButton }) => {
     if (!provider) return
     const chain = (await provider.getNetwork()).chainId
     setChainId(decToHex(chain))
-    switchOrCreateNetwork(NETWORK.chainId, NETWORK.chainName, NETWORK.ChainRpcUrl, NETWORK.chainCurrency, NETWORK.chainExplorerUrl)
+    switchOrCreateNetwork(
+      NETWORK.chainId,
+      NETWORK.chainName,
+      NETWORK.ChainRpcUrl,
+      NETWORK.chainCurrency,
+      NETWORK.chainExplorerUrl
+    );
     return [provider, address]
   }
 
