d205ac4d98d44db5ae5f857f620c0f336ff4b44d
dappsar <dapps.ar@gmail.com>
2023-11-10 21:00:51 -0300

    refactored some pages in gamma

--- src/sections/Gamma/index.jsx
+++ src/sections/Gamma/index.jsx
@@ -121,6 +121,7 @@ const index = React.forwardRef(() => {
         if (openedPack) {
           await openedPack.wait()
           setOpenPackage(true)
+          setLoaderPack(false)
           await checkNumberOfPacks()
           return openedPack
         }
@@ -133,13 +134,13 @@ const index = React.forwardRef(() => {
   }
 
   const buyPackscontact = async (numberOfPacks) => {
-    /*
-    packsContract.on('PacksPurchase', (returnValue, theEvent) => {
+    
+    gammaPacksContract.on('PackPurchase', (returnValue, theEvent) => {
+      console.log('evento PacksPurchase', returnValue)
       for (let i = 0; i < theEvent.length; i++) {
         const pack_number = ethers.BigNumber.from(theEvent[i]).toNumber()
       }
     })
-    */
 
     try {
       setLoading(true)
@@ -147,17 +148,6 @@ const index = React.forwardRef(() => {
       if (!approval) {
         await authorizeDaiContract()
       }
-      /*
-      const call = await packsContract.buyPacks(numberOfPacks, { gasLimit: 6000000 })
-        await call.wait()
-        await checkNumberOfPacks()
-        return call
-      } else {
-        const call = await packsContract.buyPacks(numberOfPacks, { gasLimit: 6000000 })
-        await call.wait()
-        return call
-      }
-      */
       const call = await gammaPacksContract.buyPacks(numberOfPacks, { gasLimit: 6000000 })
       await call.wait()
       await checkNumberOfPacks()
@@ -260,10 +250,6 @@ const index = React.forwardRef(() => {
         setCardInfo={setCardInfo}
         inventory={inventory}
         setInventory={setInventory}
-        // packsContract={packsContract}
-        // daiContract={daiContract}
-        // authorizeDaiContract={authorizeDaiContract}
-        // checkNumberOfPacks={checkNumberOfPacks}
         handleBuyPackClick={handleBuyPackClick}
       />
 
