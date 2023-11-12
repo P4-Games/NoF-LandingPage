1781b70a58722872f065527c1f295848fa2c981e
dappsar <dapps.ar@gmail.com>
2023-11-12 10:07:05 -0300

    added component loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loader_container">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loading
