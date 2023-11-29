d2cc1c8886c5d9f4d56b9c06c71ce66298034402
dappsar <dapps.ar@gmail.com>
2023-11-29 06:29:38 -0300

    chore: :coffin: remove dead code

--- src/utils/InputValidators.js
+++ src/utils/InputValidators.js
@@ -15,33 +15,3 @@ export const checkInputAddress = (address, myOwnWallet) => {
   }
   return true
 }
-
-export const checkInputArrayCardNumbers = (text, myCardNumber) => {
-  // Expresión regular para validar números y comas
-  const regex = /^([0-9]|1[0-1][0-9]|[1-9]?[0-9])(,([0-9]|1[0-1][0-9]|[1-9]?[0-9]))*$/
-
-  // Comprobar si el texto coincide con la expresión regular
-  if (!regex.test(text)) {
-    return false
-  }
-
-  // Verificar si el texto está vacío o excede el límite de 10 valores
-  const allValues = text.split(',')
-  if (allValues.length > 10 || text.trim() === '') {
-    return false
-  }
-
-  // Verificar si hay números repetidos
-  const numbers = allValues.map((val) => parseInt(val.trim(), 10))
-  const duplicates = new Set(numbers).size !== numbers.length
-  if (duplicates) {
-    return false
-  }
-
-  // Verificar si el número es myCardNumber (no debe coincidir con este número)
-  if (numbers.includes(myCardNumber)) {
-    return false
-  }
-
-  return true
-}
