ff9bc7011f476168d0c9e1a9fbdee55788bef998
dappsar <dapps.ar@gmail.com>
2023-11-29 09:39:32 -0300

    fix: :bug: update characters script path

--- src/pages/api/characters.js
+++ src/pages/api/characters.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
 
     // Obtener las rutas de las imágenes locales redimensionadas
     const characterImagePaths = characters.map((c) =>
-      join(process.cwd(), 'characters', `${c.id}.png`)
+      join(process.cwd(), 'scripts', 'characters', `${c.id}.png`)
     )
 
     // Crear una matriz de promesas para cargar las imágenes
