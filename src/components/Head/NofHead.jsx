445b84bac08b84a0f0d5aadf1009d8758b7aaba4
dappsar <dapps.ar@gmail.com>
2023-12-16 06:16:47 -0300

    [chore] :bookmark: add meta tags

--- src/components/Head/NofHead.jsx
+++ src/components/Head/NofHead.jsx
@@ -5,9 +5,14 @@ function NofHead() {
   return (
     <Head>
       <title>Number One Fan</title>
+      <meta name='author' content='P4-Games' />
       <meta name='description' content='Number One Fan' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <link rel='icon' href='/images/favicon/favicon.ico' />
+      <meta
+        name='keywords'
+        content='NoF,pixel-art,pixelart,games,blockchain,web3,ethereum,bitcoin,nft,erc20,erc721,figuritas,cartas,juegos,nostalgia,games,albums'
+      />
     </Head>
   )
 }
