e8e8c937b9dbddfcb08cecbaf544f5efd61cb751
dappsar <dapps.ar@gmail.com>
2023-12-16 06:39:42 -0300

    [feat] :sparkles: add offline page

--- src/pages/_offline.jsx
+++ src/pages/_offline.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-const OfflinePage = () => {
-
-  return (
+const OfflinePage = () => (
+  <div>
+    <p>Website Currently offline or not internet connection is available</p>
     <div>
-      <p>Website Currently offline or not internet connection is available</p>
-      <div>
-        <Link href='/' passHref>Go to Home</Link>
-      </div>
+      <Link href='/' passHref>
+        Go to Home
+      </Link>
     </div>
-  )
-}
+  </div>
+)
 
 export default OfflinePage
-
