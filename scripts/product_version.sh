404e5061e48835f0d8cc06f5270ce52f91c9c1d5
dappsar <dapps.ar@gmail.com>
2024-07-20 08:09:55 -0300

    [build] :hammer: update git scripts (#378)

---
+++ scripts/product_version.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Just set product versión in environment without TAG source code!
+# This script is used in .github\workflows\gcp.yml
+
+VERSION=""
+
+# get parameters
+while getopts v: flag
+do
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
+done
+
+# get highest tag number, and add v0.1.0 if doesn't exist
+git fetch --prune --unshallow 2>/dev/null
+CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
+
+if [[ $CURRENT_VERSION == '' ]]
+then
+  CURRENT_VERSION='v0.1.0'
+fi
+echo "Current Version: $CURRENT_VERSION"
+
+# replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+
+# get number parts
+VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+
+if [[ $VERSION == 'major' ]]
+then
+  VNUM1=v$((VNUM1+1))
+  VNUM2=0
+  VNUM3=0
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+  VNUM3=0
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
+else
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
+fi
+
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+
+# set tag in environment to use in frontend
+NOF_VERSION=$NEW_TAG
+echo "NOF_VERSION=$NEW_TAG"
+
+# write tag to GITHUB_ENV for use in GitHub Actions
+echo "NOF_VERSION=$NEW_TAG" >> $GITHUB_ENV
+
+exit 0
